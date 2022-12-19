aa07554ee5681a6a1b58e2df08c617821a145b35
mukesh <mukese0794@gmail.com>
2022-12-19 13:19:43 +0530

    MakeComponentsForLoginPage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import { sample_foods, sample_tags, sample_users } from "./data";
 import jwt from "jsonwebtoken";
-
 const app = express();
 app.use(express.json());
 app.use(cors({
@@ -43,7 +42,7 @@ app.post("/api/users/login", (req, res) => {
   const {email, password} = req.body;
   const user = sample_users.find(user => user.email === email 
     && user.password === password);
-
+    
    if(user) {
     res.send(generateTokenReponse(user));
    }
